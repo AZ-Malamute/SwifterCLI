09e788b0c1f861c88ba4efa6dfa489266762f5f7
AZ-Malamute <gasson@mac.com>
2026-05-27 00:24:17 -0400

    Swifter v0.004 - generate SwiftUI app skeleton

---
+++ SA_Inspector_001/Package.swift
@@ -0,0 +1,10 @@
+// swift-tools-version: 6.0
+import PackageDescription
+
+let package = Package(
+    name: "SA_Inspector_001",
+    platforms: [.macOS(.v15)],
+    targets: [
+        .executableTarget(name: "SA_Inspector_001")
+    ]
+)
